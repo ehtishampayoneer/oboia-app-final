23168daf26cf32bf59f7e9edbbd79c767d13b0e7
ehtishampayoneer <116853238+ehtishampayoneer@users.noreply.github.com>
2026-05-06 16:43:24 +0500

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  OBOIA
-//
-//  Registers Flutter plugins + the ARWallpaperView platform view factory.
-//
-
 import UIKit
 import Flutter
 import FirebaseCore
@@ -17,19 +10,13 @@ import FirebaseCore
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
 
-        // Firebase
         FirebaseApp.configure()
-
-        // Standard Flutter plugins
         GeneratedPluginRegistrant.register(with: self)
 
-        // Register the native AR platform view
-        guard let controller = window?.rootViewController as? FlutterViewController else {
-            fatalError("rootViewController is not FlutterViewController")
+        if let registrar = self.registrar(forPlugin: "ARWallpaperPlugin") {
+            let factory = ARWallpaperViewFactory(messenger: registrar.messenger())
+            registrar.register(factory, withId: "com.oboia/ar_view")
         }
-        let registrar = self.registrar(forPlugin: "ARWallpaperPlugin")!
-        let factory = ARWallpaperViewFactory(messenger: registrar.messenger())
-        registrar.register(factory, withId: "com.oboia/ar_view")
 
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }

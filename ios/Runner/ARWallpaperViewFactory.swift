0bbc1fabb9723d04efb3ffe4fc21d8ea99364e35
ehtishampayoneer <116853238+ehtishampayoneer@users.noreply.github.com>
2026-05-11 15:22:05 +0500

    Update ARWallpaperViewFactory.swift

--- ios/Runner/ARWallpaperViewFactory.swift
+++ ios/Runner/ARWallpaperViewFactory.swift
@@ -2,7 +2,7 @@
 //  ARWallpaperViewFactory.swift
 //  OBOIA
 //
-//  Registers ARWallpaperView as an embeddable Flutter platform view.
+//  Registers WallpaperARView as an embeddable Flutter platform view.
 //
 
 import Foundation
@@ -21,7 +21,7 @@ final class ARWallpaperViewFactory: NSObject, FlutterPlatformViewFactory {
     func create(withFrame frame: CGRect,
                 viewIdentifier viewId: Int64,
                 arguments args: Any?) -> FlutterPlatformView {
-        return ARWallpaperView(
+        return WallpaperARView(
             frame: frame,
             viewId: viewId,
             messenger: messenger,
